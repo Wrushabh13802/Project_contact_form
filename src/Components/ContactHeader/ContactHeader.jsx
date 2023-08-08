38e280986f32a02488fed9dcb5b1ae7dcc3c3402
Wrushabh Parkhi <wrushabhparkhi123@gmail.com>
2023-08-08 11:31:55 +0530

    first commit

---
+++ src/Components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,15 @@
+import styles from "./ContactHeader.module.css";
+const ContactHeader = () => {
+  return (
+    <div className={`${styles.contact_section}`}>
+      <h1>CONTACT US</h1>
+      <p>
+        WE ARE HERE TO HELP YOU OUT FILL THE CONTACT BELOW FORM AND REGISTER YOUR SELF FOR THE APP
+        WE ARE HERE TO HELP YOU OUT FILL THE CONTACT BELOW FORM AND REGISTER YOUR SELF FOR THE APP
+        .{" "}
+      </p>
+    </div>
+  );
+};
+
+export default ContactHeader;
